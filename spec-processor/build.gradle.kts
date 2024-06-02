7d90bb44fce3e03a1d4571b8d89df601aca9525e
H2 <hemoptysisheart@gmail.com>
2024-06-02 23:07:20 +0900

    🔨 프로세서 모듈에서 안드로이드 설정 삭제.

--- spec-processor/build.gradle.kts
+++ spec-processor/build.gradle.kts
@@ -1,36 +1,16 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.jvm)
 }
 
-android {
-    namespace = "com.github.hemoptysisheart.ble.spec.processor"
-    compileSdk = 34
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 
-    defaultConfig {
-        minSdk = 29
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    withSourcesJar()
 }
 
 dependencies {
     implementation(project(":spec-annotation"))
+
+    implementation(libs.ksp.symbol.processing.api)
 }
