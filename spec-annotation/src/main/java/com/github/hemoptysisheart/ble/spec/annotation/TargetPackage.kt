1af01993e792df2b432a02fdebcec7a462038a27
H2 <hemoptysisheart@gmail.com>
2024-06-08 13:50:29 +0900

    🗑 프로세서 설정이 재검토 필요함.

--- spec-annotation/src/main/java/com/github/hemoptysisheart/ble/spec/annotation/TargetPackage.kt
+++ spec-annotation/src/main/java/com/github/hemoptysisheart/ble/spec/annotation/TargetPackage.kt
@@ -5,6 +5,7 @@ package com.github.hemoptysisheart.ble.spec.annotation
  *
  * 이 어노테이션을 가진 클래스가 있는 패키지를 기준으로 스펙 코드를 생성한다.
  */
+@Deprecated("https://github.com/hemoptysisheart/android-ble/issues/4")
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class TargetPackage
--- spec-processor/src/main/java/com/github/hemoptysisheart/ble/spec/processor/config/Target.kt
+++ spec-processor/src/main/java/com/github/hemoptysisheart/ble/spec/processor/config/Target.kt
@@ -3,6 +3,7 @@ package com.github.hemoptysisheart.ble.spec.processor.config
 /**
  * [Source]의 정보로 만들 소스코드 정보.
  */
+@Deprecated("https://github.com/hemoptysisheart/android-ble/issues/4")
 data class Target(
     /**
      * 생성할 소스코드의 푸트 패키지 이름.
