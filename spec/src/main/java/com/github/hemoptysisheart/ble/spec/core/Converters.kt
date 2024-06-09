6a24eea26c2050bb46c5d6f066a953edef0a86ce
H2 <hemoptysisheart@gmail.com>
2024-06-09 12:51:31 +0900

    🎨 성능 개선을 위해 갯수가 적은 상수를 직접 비교.

--- spec/src/main/java/com/github/hemoptysisheart/ble/spec/core/Converters.kt
+++ spec/src/main/java/com/github/hemoptysisheart/ble/spec/core/Converters.kt
@@ -1,10 +1,47 @@
 package com.github.hemoptysisheart.ble.spec.core
 
 import android.bluetooth.BluetoothClass
+import android.bluetooth.BluetoothClass.Device
 
 /**
  * Android 플랫폼의 [BluetoothClass]를 [MajorDeviceClass]로 변환한다.
  */
-fun MajorDeviceClass(bleClass: BluetoothClass) = MajorDeviceClass.entries.firstOrNull { deviceClass ->
-    bleClass.majorDeviceClass == (deviceClass.value shl 8)
-} ?: MajorDeviceClass.UNCATEGORIZED
+fun MajorDeviceClass(bleClass: BluetoothClass) = when (bleClass.majorDeviceClass) {
+    Device.Major.MISC ->
+        MajorDeviceClass.MISCELLANEOUS
+
+    Device.Major.COMPUTER ->
+        MajorDeviceClass.COMPUTER
+
+    Device.Major.PHONE ->
+        MajorDeviceClass.PHONE
+
+    Device.Major.NETWORKING ->
+        MajorDeviceClass.LAN_NETWORK
+
+    Device.Major.AUDIO_VIDEO ->
+        MajorDeviceClass.AUDIO_VIDEO
+
+    Device.Major.PERIPHERAL ->
+        MajorDeviceClass.PERIPHERAL
+
+    Device.Major.IMAGING ->
+        MajorDeviceClass.IMAGING
+
+    Device.Major.WEARABLE ->
+        MajorDeviceClass.WEARABLE
+
+    Device.Major.TOY ->
+        MajorDeviceClass.TOY
+
+    Device.Major.HEALTH ->
+        MajorDeviceClass.HEALTH
+
+    Device.Major.UNCATEGORIZED ->
+        MajorDeviceClass.UNCATEGORIZED
+
+    else ->
+        throw IllegalArgumentException(
+            "unsupported majorDeviceClass : bleClass=$bleClass(bleClass.majorDeviceClass=${bleClass.majorDeviceClass})"
+        )
+}
